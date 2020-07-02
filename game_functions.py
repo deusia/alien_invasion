41d02104f32825f204b238011c02225fa171ca82
Deusia <gzc1094917614@outlook.com>
2020-07-02 20:14:01 +0800

    Initial commit

---
+++ game_functions.py
@@ -0,0 +1,41 @@
+import sys
+import pygame
+
+
+def check_keydown_events(event, ship):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        # 向右移动飞船
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """响应松开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def check_events(ship):
+    """响应案件和鼠标事件"""
+    # 监视键盘和鼠标事件
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def update_screen(ai_settings, screen, ship):
+    """更新屏幕上的图像，并切换到新的屏幕"""
+    # 每次循环都重绘屏幕
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    """存储《外星人入侵》所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕设置
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+        # 飞船的设置
+        self.ship_speed_factor = 1.5
